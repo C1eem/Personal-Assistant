56bfe418434f14e42bd56217520526b45f51894b
C1eem <ivan.kupreev03@gmail.com>
2025-07-30 13:28:00 +0400

    доделал логику collect_info

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "7720855445:AAFSyzQxBiAZI3js_xcd5q_cYxVC1aXNsqw"
-DEEP_API_TOKEN = "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"
+
 DSN = "postgresql://postgres:1234@localhost:5432/mydatabase"
